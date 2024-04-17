8a21487c334db6ca96c23e763623f9abc5a55cbd
Arthur Saito <arthur_saito1998@hotmail.com>
2024-04-16 23:55:07 -0300

    + Kafka Adapter

--- src/main/java/com/saito/payment/adapters/out/SendValidatedPaymentAdapter.java
+++ src/main/java/com/saito/payment/adapters/out/SendValidatedPaymentAdapter.java
@@ -1,4 +1,22 @@
 package com.saito.payment.adapters.out;
 
-public class SendValidatedPaymentAdapter {
+import com.saito.payment.adapters.out.message.SaleMessage;
+import com.saito.payment.application.core.domain.Sale;
+import com.saito.payment.application.core.domain.enums.SaleEvent;
+import com.saito.payment.application.ports.out.SendValidatedPaymentOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SendValidatedPaymentAdapter implements SendValidatedPaymentOutputPort {
+
+    @Autowired
+    private KafkaTemplate<String, SaleMessage> kafkaTemplate;
+
+    @Override
+    public void send(Sale aSale, SaleEvent event) {
+        var saleMessage = new SaleMessage(aSale, event);
+        kafkaTemplate.send("tp-saga-sale", saleMessage);
+    }
 }
--- src/main/java/com/saito/payment/adapters/out/message/SaleMessage.java
+++ src/main/java/com/saito/payment/adapters/out/message/SaleMessage.java
@@ -1,4 +1,16 @@
 package com.saito.payment.adapters.out.message;
 
+import com.saito.payment.application.core.domain.Sale;
+import com.saito.payment.application.core.domain.enums.SaleEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaleMessage {
+
+    private Sale sale;
+    private SaleEvent saleEvent;
 }
